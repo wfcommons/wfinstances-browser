b12d089fa231302727f515875e9f2c9252b267be
Kenji Sanehira <kenjisan@hawaii.edu>
2024-01-25 15:06:23 -1000

    Initialized basic Navbar

---
+++ ui/app/components/Navbar.tsx
@@ -0,0 +1,51 @@
+import { useState } from 'react';
+import { Container, Group, Burger, Image, Text } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
+import classes from './Navbar.module.css';
+
+const links = [
+  { link: '/about', label: 'Features' },
+  { link: '/pricing', label: 'Pricing' },
+  { link: '/learn', label: 'Learn' },
+  { link: '/community', label: 'Community' },
+];
+
+export function Navbar() {
+  const [opened, { toggle }] = useDisclosure(false);
+  const [active, setActive] = useState(links[0].link);
+
+  const items = links.map((link) => (
+    <a
+      key={link.label}
+      href={link.link}
+      className={classes.link}
+      data-active={active === link.link || undefined}
+      onClick={(event) => {
+        event.preventDefault();
+        setActive(link.link);
+      }}
+    >
+      {link.label}
+    </a>
+  ));
+
+  return (
+    <header className={classes.header}>
+      <Container size="md" className={classes.inner}>
+        <Group gap={5}>
+          <Image
+              radius='xs'
+               h={25}
+              src="https://avatars.githubusercontent.com/u/80002382?s=200&v=4"
+          />
+          <Text fw={500}>WfCommons</Text>
+        </Group>
+        <Group gap={5} visibleFrom="xs">
+          {items}
+        </Group>
+
+        <Burger opened={opened} onClick={toggle} hiddenFrom="xs" size="sm" />
+      </Container>
+    </header>
+  );
+}
