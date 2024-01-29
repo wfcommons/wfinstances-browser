4f0dce58ef38d09dc42e1777eaabf83aae035327
Tevin Takata <takatat9@hawaii.edu>
2024-01-29 03:30:17 -1000

    Setup ESLint
    Added ESLint rules. Still trying to get it to work with Meteor. Changed "name" in package.json file so that it's a valid name

--- ui/.eslintrc.cjs
+++ ui/.eslintrc.cjs
@@ -1,9 +1,3 @@
-/**
- * This is intended to be a basic starting point for linting in your app.
- * It relies on recommended configs out of the box for simplicity, but you can
- * and should modify this configuration to best suit your team's needs.
- */
-
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
   root: true,
@@ -18,21 +12,32 @@ module.exports = {
     browser: true,
     commonjs: true,
     es6: true,
+    node: true,
   },
 
   // Base config
-  extends: ["eslint:recommended"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:meteor/recommended",
+    "airbnb",
+  ],
 
   overrides: [
     // React
     {
       files: ["**/*.{js,jsx,ts,tsx}"],
-      plugins: ["react", "jsx-a11y"],
+      plugins: ["react", "jsx-a11y", "meteor"],
       extends: [
         "plugin:react/recommended",
         "plugin:react/jsx-runtime",
         "plugin:react-hooks/recommended",
         "plugin:jsx-a11y/recommended",
+        "plugin:react/recommended",
+        "plugin:meteor/recommended",
+        "airbnb",
       ],
       settings: {
         react: {
@@ -52,7 +57,7 @@ module.exports = {
     // Typescript
     {
       files: ["**/*.{ts,tsx}"],
-      plugins: ["@typescript-eslint", "import"],
+      plugins: ["@typescript-eslint", "import", "meteor"],
       parser: "@typescript-eslint/parser",
       settings: {
         "import/internal-regex": "^~/",
@@ -69,6 +74,9 @@ module.exports = {
         "plugin:@typescript-eslint/recommended",
         "plugin:import/recommended",
         "plugin:import/typescript",
+        "plugin:react/recommended",
+        "plugin:meteor/recommended",
+        "airbnb",
       ],
     },
 
@@ -80,4 +88,36 @@ module.exports = {
       },
     },
   ],
+
+  rules: {
+    'arrow-parens': 'off',
+    camelcase: 'off',
+    'class-methods-use-this': 'off',
+    'func-names': 'off',
+    'import/no-absolute-path': 'off',
+    'import/no-unresolved': 'off',
+    'import/extensions': 'off',
+    'import/imports-first': 'off',
+    'import/prefer-default-export': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    indent: ['error', 2],
+    'linebreak-style': 'off',
+    'max-len': ['error', 250],
+    'meteor/eventmap-params': [2, { eventParamName: 'event', templateInstanceParamName: 'instance' }],
+    'meteor/template-names': 'off',
+    'no-confusing-arrow': ['error', { allowParens: true }],
+    'no-plusplus': 'off',
+    'no-underscore-dangle': 'off',
+    'object-curly-newline': 'off',
+    'object-property-newline': 'off',
+    'object-shorthand': 'off',
+    'operator-linebreak': 'off',
+    'padded-blocks': 'off',
+    'prefer-arrow-callback': 'off',
+    'prefer-destructuring': 'off',
+    'prefer-promise-reject-errors': 'off',
+    'react/function-component-definition': [2, { namedComponents: 'arrow-function' }],
+    'react/jsx-one-expression-per-line': 'off',
+    'react/no-array-index-key': 'off',
+  },
 };
