f7ee9918256c7573d0ac3f512d2bb241b83badf4
Kenji Sanehira <kenjisan@hawaii.edu>
2024-01-29 23:35:23 -1000

    Attempt to use loader with Basic Example.

--- ui/app/components/Main.tsx
+++ ui/app/components/Main.tsx
@@ -1,20 +1,9 @@
-import { useEffect, useMemo, useState } from 'react';
+import { useMemo } from 'react';
 import {
   MantineReactTable,
   useMantineReactTable,
   type MRT_ColumnDef,
-  type MRT_ColumnFiltersState,
-  type MRT_PaginationState,
-  type MRT_SortingState,
 } from 'mantine-react-table';
-
-
-type UserApiResponse = {
-  data: Array<Workflow>;
-  meta: {
-    totalRowCount: number;
-  };
-};
 
 type Workflow = {
   id: string;
@@ -29,140 +18,60 @@ type Workflow = {
   maxWidth: number;
 };
 
-const Table = () => {
-  //data and fetching state
-  const [data, setData] = useState<Workflow[]>([]);
-  const [isError, setIsError] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
-  const [isRefetching, setIsRefetching] = useState(false);
-  const [rowCount, setRowCount] = useState(0);
+export function Main( fetchedData: any ) {
+const data: Workflow[] = fetchedData;
+console.log(data);
+const columns = useMemo<MRT_ColumnDef<Workflow>[]>(
+  () => [
+    {
+      accessorKey: 'id',
+      header: 'id',
+    },
 
-  //table state
-  const [columnFilters, setColumnFilters] = useState<MRT_ColumnFiltersState>(
-    [],
-  );
-  const [globalFilter, setGlobalFilter] = useState('');
-  const [sorting, setSorting] = useState<MRT_SortingState>([]);
-  const [pagination, setPagination] = useState<MRT_PaginationState>({
-    pageIndex: 0,
-    pageSize: 10,
-  });
-
-  //if you want to avoid useEffect, look at the React Query example instead
-  useEffect(() => {
-    const fetchData = async () => {
-      if (!data.length) {
-        setIsLoading(true);
-      } else {
-        setIsRefetching(true);
-      }
-
-      try {
-        const response = await fetch('http://localhost:8081/wf-instance-metrics', {mode: 'cors'});
-        const json = (await response.json()) as UserApiResponse;
-        setData(json.data);
-        setRowCount(json.meta.totalRowCount);
-      } catch (error) {
-        setIsError(true);
-        console.error(error);
-        return;
-      }
-      setIsError(false);
-      setIsLoading(false);
-      setIsRefetching(false);
-    };
-    fetchData();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [
-    columnFilters, //refetch when column filters change
-    globalFilter, //refetch when global filter changes
-    pagination.pageIndex, //refetch when page index changes
-    pagination.pageSize, //refetch when page size changes
-    sorting, //refetch when sorting changes
-  ]);
-
-  const columns = useMemo<MRT_ColumnDef<Workflow>[]>(
-    () => [
-      {
-        accessorKey: 'id',
-        header: 'id',
-      },
-
-      {
-        accessorKey: 'githubRepo',
-        header: 'Github Repository',
-      },
-      {
-        accessorKey: 'numTasks',
-        header: 'Number of Tasks',
-      },
-      {
-        accessorKey: 'numFiles',
-        header: 'Number of Files',
-      },
-      {
-        accessorKey: 'totalBytesRead',
-        header: 'Total Bytes Read',
-      },
-      {
-        accessorKey: 'totalBytesWritten',
-        header: 'Total Bytes Written',
-      },
-      {
-        accessorKey: 'work',
-        header: 'Total Work',
-      },
-      {
-        accessorKey: 'depth',
-        header: 'Depth of Workflow',
-      },
-      {
-        accessorKey: 'minWidth',
-        header: 'Minimum Width',
-      },
-      {
-        accessorKey: 'maxWidth',
-        header: 'Maximum Width',
-      },
-    ],
-    [],
-  );
+    {
+      accessorKey: 'githubRepo',
+      header: 'Github Repository',
+    },
+    {
+      accessorKey: 'numTasks',
+      header: 'Number of Tasks',
+    },
+    {
+      accessorKey: 'numFiles',
+      header: 'Number of Files',
+    },
+    {
+      accessorKey: 'totalBytesRead',
+      header: 'Total Bytes Read',
+    },
+    {
+      accessorKey: 'totalBytesWritten',
+      header: 'Total Bytes Written',
+    },
+    {
+      accessorKey: 'work',
+      header: 'Total Work',
+    },
+    {
+      accessorKey: 'depth',
+      header: 'Depth of Workflow',
+    },
+    {
+      accessorKey: 'minWidth',
+      header: 'Minimum Width',
+    },
+    {
+      accessorKey: 'maxWidth',
+      header: 'Maximum Width',
+    },
+  ],
+  [],
+);
 
   const table = useMantineReactTable({
     columns,
-    data,
-    enableRowSelection: true,
-    getRowId: (row) => row.id,
-    initialState: { showColumnFilters: true },
-    manualFiltering: true,
-    manualPagination: true,
-    manualSorting: true,
-    rowCount,
-    onColumnFiltersChange: setColumnFilters,
-    onGlobalFilterChange: setGlobalFilter,
-    onPaginationChange: setPagination,
-    onSortingChange: setSorting,
-    state: {
-      columnFilters,
-      globalFilter,
-      isLoading,
-      pagination,
-      showAlertBanner: isError,
-      showProgressBars: isRefetching,
-      sorting,
-    },
-    mantineToolbarAlertBannerProps: isError
-      ? { color: 'red', children: 'Error loading data' }
-      : undefined,
+    data, //must be memoized or stable (useState, useMemo, defined outside of this component, etc.)
   });
 
   return <MantineReactTable table={table} />;
 };
-
-export default Table;
-
-export function Main() {
-  return (
-    <Table/>
-  );
-}
--- ui/app/routes/_index.tsx
+++ ui/app/routes/_index.tsx
@@ -1,7 +1,23 @@
 import type { MetaFunction } from "@remix-run/node";
+import { Container } from '@mantine/core';
 import { Navbar } from "~/components/Navbar";
 import { Footer } from "~/components/Footer";
 import { Main } from "~/components/Main";
+import { json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+
+type Workflow = {
+  id: string;
+  githubRepo: string;
+  numTasks: number;
+  numFiles: number;
+  totalBytesRead: string;
+  totalBytesWritten: string;
+  work: string;
+  depth: number;
+  minWidth: number;
+  maxWidth: number;
+};
 
 export const meta: MetaFunction = () => {
   return [
@@ -9,11 +25,22 @@ export const meta: MetaFunction = () => {
   ];
 };
 
+export const loader = async () => {
+  const response = await fetch('http://localhost:8081/wf-instance-metrics/epigenomics-chameleon-ilmn-4seq-100k-001.json');
+  const workflows: Workflow[] = await response.json();
+  return json({ workflows });
+};
+
 export default function Index() {
+  const { workflows } = useLoaderData<typeof loader>();
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
       <Navbar/>
-        <Main/>
+        <Container fluid>
+          <Main
+            fetchedData= {workflows}
+          />
+        </Container>
       <Footer/>
     </div>
   );
