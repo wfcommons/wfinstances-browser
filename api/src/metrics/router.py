e27c273ea05dd05b79339cee5c92290a3f2fc6af
Jordan Wong <jordanw4@hawaii.edu>
2024-02-04 16:46:56 -1000

    Reorganize file structure and routers

--- api/src/metrics/router.py
+++ api/src/metrics/router.py
@@ -2,15 +2,28 @@ from fastapi import APIRouter
 
 from src.database import wf_instance_metrics_collection, wf_instance_collection
 from src.metrics.schema import serialize_wf_instances_metrics, serialize_wf_instance_metrics
+from src.metrics.service import generate_graph_metrics
+from src.wfinstances.schema import serialize_wf_instance
 
 router = APIRouter()
 
 
-@router.get('/wf-instance-metrics')
+@router.get('/')
 async def get_wf_instance_metrics():
     return serialize_wf_instances_metrics(wf_instance_metrics_collection.find())
 
 
-@router.get('/wf-instance-metrics/{id}')
+@router.get('/{id}')
 async def get_wf_instance_metrics(id: str):
     return serialize_wf_instance_metrics(wf_instance_metrics_collection.find_one({'_id': id}))
+
+
+@router.get('/{id}/metrics')
+async def get_wf_instance_metrics_metrics(id: str):
+    wf_instance_metrics = serialize_wf_instance(wf_instance_collection.find_one({'_id': id}))
+    depth, min_width, max_width = generate_graph_metrics(wf_instance_metrics['workflow']['tasks'])
+    return {
+        'depth': depth,
+        'min_width': min_width,
+        'max_width': max_width
+    }
--- api/src/metrics/service.py
+++ api/src/metrics/service.py
@@ -1,4 +1,3 @@
-import re
 from collections import Counter, defaultdict, deque
 from datetime import timedelta
 from src.metrics.graph import Graph
@@ -6,22 +5,22 @@ from src.metrics.graph import Graph
 
 def generate_metrics(wf_instance):
     tasks = wf_instance['workflow']['tasks']
-    num_tasks, num_files, total_bytes_read, total_bytes_written, work = generate_list_metrics(tasks)
-    depth, min_width, max_width = generate_graph_metrics(tasks)
+    num_tasks, num_files, total_bytes_read, total_bytes_written, work = _generate_list_metrics(tasks)
+    depth, min_width, max_width = _generate_graph_metrics(tasks)
 
     return {
         'numTasks': num_tasks,
         'numFiles': num_files,
-        'totalBytesRead': get_bytes_string(total_bytes_read),
-        'totalBytesWritten': get_bytes_string(total_bytes_read),
-        'work': get_time_string(work),
+        'totalBytesRead': _get_bytes_string(total_bytes_read),
+        'totalBytesWritten': _get_bytes_string(total_bytes_read),
+        'work': _get_time_string(work),
         'depth': depth,
         'minWidth': min_width,
         'maxWidth': max_width
     }
 
 
-def generate_list_metrics(tasks):
+def _generate_list_metrics(tasks):
     files, total_bytes_read, total_bytes_written, work = set(), 0, 0, 0
 
     for task in tasks:
@@ -34,7 +33,7 @@ def generate_list_metrics(tasks):
     return len(tasks), len(files), total_bytes_read, total_bytes_written, work
 
 
-def generate_graph_metrics(tasks):
+def _generate_graph_metrics(tasks):
     # Build graph
     tasks_graph, input_tasks = Graph(), []
     for task in tasks:
@@ -48,19 +47,17 @@ def generate_graph_metrics(tasks):
         tasks_graph, input_tasks = Graph(), []
         input_files, output_files = {}, {}
         for index, task in enumerate(tasks):
-            num_input = 0
             for file in task['files']:
                 if file['link'] == 'input':
                     input_files[file['name']] = task['name'] + str(index)
-                    num_input += 1
                 elif file['link'] == 'output':
                     output_files[file['name']] = task['name'] + str(index)
-            if num_input == 0:
-                input_tasks.append(task['name'] + str(index))
 
         for key in list(input_files.keys()):
             if key in output_files:
                 tasks_graph.add_edge(output_files[key], input_files[key])
+            else:
+                input_tasks.append(input_files[key])
 
     # Calculate levels and depth
     depth, levels = 0, defaultdict(int)
@@ -84,13 +81,13 @@ def generate_graph_metrics(tasks):
     return depth, min_width, max_width
 
 
-def get_bytes_string(size):
+def _get_bytes_string(size):
     for unit in ['KB', 'MB', 'GB']:
         size = size / 1000
         if size < 1000:
             return '{0:.2f} {1}'.format(size, unit)
 
 
-def get_time_string(seconds):
+def _get_time_string(seconds):
     td = str(timedelta(seconds=seconds)).split(':')
     return f'{td[0]}h{td[1]}m{float(td[2]):.0f}s'
