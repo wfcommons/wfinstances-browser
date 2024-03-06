5cbec172cd1ebd942b8a732dcbf0effd9233d0a9
Kenji Sanehira <kenjisan@hawaii.edu>
2024-03-05 14:38:29 -1000

    Created Test Modal

--- ui/app/components/MetricsTable.tsx
+++ ui/app/components/MetricsTable.tsx
@@ -1,4 +1,4 @@
-import { useMemo } from 'react';
+import { SetStateAction, useMemo, useState } from 'react';
 import {
   MantineReactTable,
   useMantineReactTable,
@@ -6,9 +6,16 @@ import {
   MRT_GlobalFilterTextInput,
   MRT_ToggleFiltersButton,
   MRT_ShowHideColumnsButton,
+  MRT_Icons,
 } from 'mantine-react-table';
-import { Box, Flex, Menu } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
+import { Box, Button, Flex, Menu, Modal } from '@mantine/core';
+import { IconGraph } from '@tabler/icons-react';
+import classes from './style/Navbar.module.css';
+import cx from 'clsx';
+import 'mantine-react-table/styles.css';
 import { Download } from './Download';
+
 
 export type Metrics = {
   id: string;
@@ -23,32 +30,96 @@ export type Metrics = {
   maxWidth: number;
 };
 
-function formatBytes(bytes: number) {
-  if (bytes == 0) return '0 Bytes';
-  const k = 1024;
-  const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
-  const i = Math.floor(Math.log(bytes) / Math.log(k));
-  return parseFloat((bytes / Math.pow(k, i)).toFixed(2)) + " " + sizes[i];
+type WfInstance = {
+  id?: string
+  name: string,
+  description: string,
+  createdAt: string,
+  schemaVersion: string,
+  wms: {
+    name: string,
+    version: string,
+    url: string
+  },
+  author: {
+    name: string,
+    email: string,
+    institution: string,
+    country: string
+  }
+  workflow: {
+    makespanInSeconds: number,
+    executedAt: string,
+    machines: {
+      system: string,
+      architecture: string,
+      nodeName: string,
+      release: string,
+      memoryInBytes: number,
+      cpu: {
+        count: number,
+        speed: number,
+        vendor: string
+      }
+    }[],
+    tasks: {
+      name: string,
+      id: string,
+      category: string,
+      type: string,
+      command: {
+        program: string,
+        arguments: string[]
+      },
+      parents: string[],
+      files: {
+        name: string,
+        sizeInBytes: number,
+        link: string
+      }[],
+      runtimeInSeconds: number,
+      cores: number,
+      avgCPU: number,
+      readBytes: number,
+      writtenBytes: number,
+      memoryInBytes: number,
+      energy: number,
+      avgPower: number,
+      priority: number,
+      machine: string
+    }[]
+  }
+};
+
+function formatBytes(bytes: number, unit: string) {
+  const units: { [ key: string ]: number } = {
+    "B": 1,
+    "KB": 1024,
+    "MB": 1024 **2,
+    "GB": 1024 **3,
+    "TB": 1024 **4
+  };
+
+  if (unit in units) {
+    return (bytes / units[unit]).toFixed(2);
+  } else {
+    throw new Error("Invalid Unit provided to function: " + unit + " needs to be changed.");
+  }
 }
 
-function formatWork(work: number) {
-  const days = Math.floor(work / (3600 * 24));
-  const hours = Math.floor((work % (3600 * 24)) / 3600);
-  const minutes = Math.floor((work % 3600) / 60);
-  const seconds = Math.floor(work % 60);
+function formatWork(work: number, unit: string) {
+  const unitsInSeconds:{[key: string]: number} = {
+    'sec': 1,
+    'min': 60,
+    'hr': 3600,
+    'day(s)':86400
+  }
 
-  let result = '';
-  if (days > 0) {
-    result += days + 'd';
+  if(unit in unitsInSeconds) {
+    return (work / unitsInSeconds[unit]).toFixed(2);
+  } else {
+    throw new Error("Invalid unit provided to function: " + unit + " needs to be changed.")
   }
-  if (hours > 0 || days > 0) {
-    result += hours + 'h';
-  }
-  if (minutes > 0 || days > 0 || hours > 0) {
-    result += minutes + 'm';
-  }
-  result += seconds + 's';
-  return result.trim();
 }
 
 export function MetricsTable({
@@ -56,7 +127,13 @@ export function MetricsTable({
 } : {
   data: Metrics[]
 }) {
-  // Columns to be used in the table.
+  // TODO Replace these temporary filtering values with a way to sort based on ByteUnit and WorkUnit
+  const testByteUnit = 'MB';
+  const testWorkUnit = 'min';
+
+  const [opened, { open, close}] = useDisclosure(false);
+  
+// Columns to be used in the table.
   const columns = useMemo<MRT_ColumnDef<Metrics>[]>(
     () => [
       {
@@ -101,35 +178,38 @@ export function MetricsTable({
             filterFn: 'between',
           },
           {
-            accessorKey: 'totalBytesRead',
+            accessorFn: (row) => {return formatBytes(row.totalBytesRead, testByteUnit)},
+            id: 'totalBytesRead',
             header: 'Total Bytes Read',
             size: 50,
-            columnFilterModeOptions: ['fuzzy', 'contains', 'endsWith'],
+            columnFilterModeOptions: ['fuzzy', 'contains', 'between', 'betweenInclusive'],
             Cell: ({ cell }) =>(
               <Box>
-                {formatBytes(cell.getValue<number>())}
+                {cell.getValue<number>() + " " + testByteUnit}
               </Box>
             )
           },
           {
-            accessorKey: 'totalBytesWritten',
+            accessorFn: (row) => {return formatBytes(row.totalBytesWritten, testByteUnit)},
+            id: 'totalBytesWritten',
             header: 'Total Bytes Written',
             size: 50,
-            columnFilterModeOptions: ['fuzzy', 'contains', 'endsWith',],
+            columnFilterModeOptions: ['fuzzy', 'contains', 'between', 'betweenInclusive'],
             Cell: ({ cell }) =>(
               <Box>
-                {formatBytes(cell.getValue<number>())}
+                {cell.getValue<number>() + " " + testByteUnit}
               </Box>
             )
           },
           {
-            accessorKey: 'work',
+            accessorFn: (row) => {return formatWork(row.work, testWorkUnit)},
+            id: 'work',
             header: 'Total Work',
             size: 30,
             columnFilterModeOptions: ['fuzzy', 'contains', 'startsWith',],
             Cell: ({ cell }) =>(
               <Box>
-                {formatWork(cell.getValue<number>())}
+                {cell.getValue<number>() + " " + testWorkUnit}
               </Box>
             )
           },
@@ -160,9 +240,14 @@ export function MetricsTable({
     [],
   );
 
+  const faIcons: Partial<MRT_Icons> = {
+    IconDots: () => <IconGraph className={cx(classes.icon, classes.light)} stroke={1.5}/>
+  }
+
   const table = useMantineReactTable({
     columns,
     data,
+    icons: faIcons,
     enableColumnFilterModes: true,
     enableColumnDragging: false,
     enableFacetedValues:true,
@@ -183,7 +268,8 @@ export function MetricsTable({
         depth: false,
         minWidth: false,
         maxWidth: false,
-      }
+      },
+      pagination: { pageIndex: 0, pageSize: 10 }
     },
     paginationDisplayMode: 'pages',
     positionToolbarAlertBanner: 'bottom',
@@ -196,25 +282,27 @@ export function MetricsTable({
     },
     renderRowActionMenuItems: () => (
       <>
-        <Menu.Item>Visualize Workflow</Menu.Item>
+        <Menu.Item onClick={open}>Visualize Workflow</Menu.Item>
       </>
     ), 
     renderTopToolbar: ({ table }) => {
       return (
         <Flex p="md" justify="space-between">
-          <Flex gap="xs">
-            {/* import MRT sub-components */}
-            <MRT_GlobalFilterTextInput table={table} />
-            <MRT_ToggleFiltersButton table={table} />
-            <MRT_ShowHideColumnsButton table={table}/>
-          </Flex>
           <Flex>
             <Download table={table} />
+          </Flex>
+          <Flex gap="xs">
+            {/* import MRT sub-components */}
+            <MRT_ToggleFiltersButton table={table} />
+            <MRT_ShowHideColumnsButton table={table}/>
+            <MRT_GlobalFilterTextInput table={table} />
           </Flex>
         </Flex>
       );
     }
   });
 
-  return <MantineReactTable table={table} />;
+  return (
+  <><Modal opened={opened} onClose={close} title="WfMetric Visualization" size="70%">TEST</Modal><MantineReactTable table={table} /></>
+  );
 };
