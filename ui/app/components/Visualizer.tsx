8ae942ea3ca3c7c528a4b6668f5ebf076f63ac1b
Kenji Sanehira <kenjisan@hawaii.edu>
2024-03-13 14:29:10 -1000

    Temp Graph Variable Population

--- ui/app/components/Visualizer.tsx
+++ ui/app/components/Visualizer.tsx
@@ -1,4 +1,6 @@
+import { IconTallymarks } from '@tabler/icons-react';
 import Cytoscape from 'cytoscape';
+import { useState } from 'react';
 import CytoscapeComponent from 'react-cytoscapejs';
 
 export function Visualizer({ 
@@ -6,11 +8,21 @@ export function Visualizer({
   }: {
     id: String
   }) {
-    fetch(`localhost:8081/wf-instances/${id}`)
-    .then(res => res.json())
-
+    const [elements, setElements] = useState([]);
+        const tasks = fetch(`http://localhost:8081/wf-instances/${id}`)
+        .then(res => res.json())
+        .then(res => {
+            const tasks = res.result.workflow.tasks;
+            const graphElements = [];
+            tasks.forEach((task: any, i: number) => {
+                graphElements.push({data: { id: i, label: task.name }})
+            });
+            setElements(graphElements);
+        })
+       
+        
     return (
         // Test return
-        <CytoscapeComponent elements={[{data: { id: 'a' }}]} style={{ minWidth: '400px', maxWidth: '1250px', height: '700px' }} />
+        <CytoscapeComponent elements={elements} style={{ minWidth: '400px', maxWidth: '1250px', height: '700px' }} />
     );
   }
