16bbd26c414d88bb1c779a4fdf85ec028007cb55
Henri Casanova <henric@hawaii.edu>
2024-07-30 16:24:06 -1000

    Tried to deal with CORS Replaced a localhost in the URL to the API with the container name

--- api/src/main.py
+++ api/src/main.py
@@ -9,13 +9,19 @@ app = FastAPI(swagger_ui_parameters={"displayRequestDuration": True})
 app.include_router(wf_instances_router, prefix='/wf-instances')
 app.include_router(metrics_router, prefix='/metrics')
 app.add_exception_handler(GithubResourceNotFoundException, github_resource_not_found_exception_handler)
+
+# origins=['http://localhost:8080']  # Original code
+origins = ["http://wfinstances-browser-ui:8080", "http://localhost:8080", "http://wfinstances-browser-nginx:80", "http://wfinstances-browser-nginx"]
+# origins = ["*"]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['http://localhost:8080'],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
 )
 
 if __name__ == '__main__':
+    import sys
     uvicorn.run('main:app', host='localhost', port=8081, reload=True)
