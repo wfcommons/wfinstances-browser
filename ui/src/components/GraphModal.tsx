84fa2d47a1f831086c00fef885bb169489dd9672
Henri Casanova <henric@hawaii.edu>
2024-07-31 18:38:35 -1000

    Bug--

--- ui/src/components/GraphModal.tsx
+++ ui/src/components/GraphModal.tsx
@@ -132,7 +132,8 @@ export function GraphModal({
         refetchOnWindowFocus: false,
         queryKey: ['id', id],
         queryFn: () => 
-            fetch(`http://wfinstances-browser-api:8081/wf-instances/${id}`)
+            // fetch(`http://wfinstances-browser-api:8081/wf-instances/${id}`)
+            fetch(`http://localhost:8081/wf-instances/${id}`)
                 .then(res => res.json())
                 .then(res => buildGraphElements(res.result))
     });
