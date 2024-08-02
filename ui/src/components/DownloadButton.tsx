55c4e54accbaaa04394d1eb7a1547e2b50d57ccc
Henri Casanova <henric@hawaii.edu>
2024-08-01 21:13:32 -1000

    Modified routes with some private/ and some public/

--- ui/src/components/DownloadButton.tsx
+++ ui/src/components/DownloadButton.tsx
@@ -32,7 +32,7 @@ export function DownloadButton({
         enabled: false,
         queryKey: ['ids', ids],
         queryFn: () => 
-            fetch('http://localhost/wf-instances', {
+            fetch('http://localhost/wf-instances/public/', {
                 method: 'POST',
                 headers: {                              
                     'Content-Type': 'application/json',
