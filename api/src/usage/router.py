2dfdb42baf0339d83c96f1aee90b5eae86608bca
Henri Casanova <henric@hawaii.edu>
2024-08-02 17:26:28 -1000

    Implemented usage retrieval

--- api/src/usage/router.py
+++ api/src/usage/router.py
@@ -1,20 +1,23 @@
 from fastapi import APIRouter
 from src.database import usage_collection
 from src.models import ApiResponse
-import sys
+from bson.json_util import dumps
+
 
 router = APIRouter()
 
 @router.get('/public/', response_model=ApiResponse)
-async def get_number_of_instances_downloaded() -> int:
+async def get_number_of_instances_downloaded() -> dict:
     """
     Get all the number_of_instances_downloaded.
     """
     usage = usage_collection.find_one({})
+    # TODO: Find a way to convert the usage variable to a dict... should be one weird...!!
     return {
-        'detail': ('Download count retrieved.'
-                   if len(usage) > 0 else
-                   'No download count retrieved.'),
-        'result': usage["count"]
+        'detail': ('Usage data retrieved.'
+                   if usage else
+                   'No usage data retrieved.'),
+        'result': {"download_count": usage['download_count'],
+                   "viz_count": usage['viz_count']}
     }
 
