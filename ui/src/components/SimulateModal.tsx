92c2e7420bb95824dfe25a06ccd541ef4716f76f
Alyoola <808alyssag@gmail.com>
2024-10-21 17:25:52 -1000

    Fixed UI design

--- ui/src/components/SimulateModal.tsx
+++ ui/src/components/SimulateModal.tsx
@@ -1,4 +1,4 @@
-import {Button, Group, Modal, rem, Slider, Tabs, Tooltip, UnstyledButton, Text, Fieldset} from '@mantine/core';
+import {Button, Group, Modal, rem, Slider, Tabs, Tooltip, UnstyledButton, Text, Fieldset, SimpleGrid} from '@mantine/core';
 import { IconX, IconPlus } from '@tabler/icons-react';
 import {simulate} from '../../workflow_simulator/simulator';
 import {useState} from "react";
@@ -15,9 +15,9 @@ export function SimulateModal({
 }) {
     const iconStyle = { width: rem(12), height: rem(12) };
     const [tabs, setTabs] = useState([
-        { id: '1', label: 'Cluster 1', content: 'Content of Cluster 1' },
-        { id: '2', label: 'Cluster 2', content: 'Content of Cluster 2' },
-        { id: '3', label: 'Cluster 3', content: 'Content of Cluster 3' },
+        { id: '1', label: 'Cluster 1' },
+        { id: '2', label: 'Cluster 2' },
+        { id: '3', label: 'Cluster 3' },
     ]);
     const [tabTracker, increaseTracker] = useState(tabs.length+1);
 
@@ -28,7 +28,7 @@ export function SimulateModal({
     const addTab = () => {
         increaseTracker(tabTracker + 1);
         const newId = (tabTracker).toString();
-        const newTab = { id: newId, label: `Cluster ${newId}`, content: `Content of Cluster ${newId}` };
+        const newTab = { id: newId, label: `Cluster ${newId}` };
         setTabs([...tabs, newTab]);
         setActiveTab(newId); // Set focus on the new tab
     };
@@ -53,59 +53,68 @@ export function SimulateModal({
             <Group justify="center" pt={15}>
                 <Button variant="default" onClick={() => simulate(id)}>Run Simulation</Button>
             </Group>
-            <Group justify="center" pt={15}>
-                <Fieldset legend="Disk Specifications">
-                    <Text size="sm" mt="xl">Read Bandwidth</Text>
-                    <Slider defaultValue={100} min={50} max={1000} step={50} label={(value) => `${value} MBps`} />
-                    <Text size="sm" mt="xl">Write Bandwidth</Text>
-                    <Slider defaultValue={100} min={50} max={1000} step={50} label={(value) => `${value} MBps`} />
-                </Fieldset>
-            </Group>
-            <Group justify="center" pt={10}>
-                <Tabs defaultValue="cluster">
-                    <Tabs.List>
-                        {tabs.map((tab) => (
-                            <Tabs.Tab key={tab.id} value={tab.id} rightSection={<UnstyledButton
-                                onClick={(e) => {
-                                    e.stopPropagation();
-                                    removeTab(tab.id);
-                                }}>
-                                <IconX style={iconStyle} />
-                            </UnstyledButton>}>
-                                {tab.label}
-                            </Tabs.Tab>
-                        ))}
-                        <Tooltip label={"Add Cluster"}>
-                            <UnstyledButton onClick={addTab}><IconPlus style={iconStyle} /></UnstyledButton>
-                        </Tooltip>
-                    </Tabs.List>
+            <Group pr={190} pl={190} pt={15} justify="center" grow>
+                <Fieldset legend="Simulation Specifications">
+                    <Group justify="center" pt={15} pl={160} pr={160} grow>
+                        <Fieldset legend="Disk Specifications">
+                            <Text size="sm" mt="xl">Read Bandwidth</Text>
+                            <Slider defaultValue={100} min={50} max={1000} step={50} label={(value) => `${value} MBps`} />
+                            <Text size="sm" mt="xl">Write Bandwidth</Text>
+                            <Slider defaultValue={100} min={50} max={1000} step={50} label={(value) => `${value} MBps`} />
+                        </Fieldset>
+                    </Group>
+                    <Group justify="center" pt={10} pl={15} pr={15} grow>
+                        <Tabs defaultValue="1">
+                            <Tabs.List>
+                                {tabs.map((tab) => (
+                                    <Tabs.Tab key={tab.id} value={tab.id} rightSection={<UnstyledButton
+                                        onClick={(e) => {
+                                            e.stopPropagation();
+                                            removeTab(tab.id);
+                                        }}>
+                                        <IconX style={iconStyle} />
+                                    </UnstyledButton>}>
+                                        {tab.label}
+                                    </Tabs.Tab>
+                                ))}
+                                <Tooltip label={"Add Cluster"}>
+                                    <UnstyledButton onClick={addTab}><IconPlus style={iconStyle} /></UnstyledButton>
+                                </Tooltip>
+                            </Tabs.List>
 
-                    {tabs.map((tab) => (
-                        <Tabs.Panel key={tab.id} value={tab.id}>
-                            {tab.content}
-                            <Fieldset legend="Cluster Link Specifications">
-                                <Text size="sm" mt="xl">Bandwidth</Text>
-                                <Slider defaultValue={100} min={50} max={1000} step={50} label={(value) => `${value} kBps`} />
-                                <Text size="sm" mt="xl">Latency</Text>
-                                <Slider defaultValue={10} min={1} max={100} step={1} label={(value) => `${value} ms`} />
-                            </Fieldset>
-                            <Fieldset legend="Compute Node Specifications">
-                                <Text size="sm" mt="xl">Number of Compute Nodes</Text>
-                                <Slider defaultValue={1} min={1} max={10} step={1} />
-                                <Text size="sm" mt="xl">Cores per Compute Node</Text>
-                                <Slider
-                                    defaultValue={50}
-                                    label={(val) => marks.find((mark) => mark.value === val)!.label}
-                                    step={25}
-                                    marks={marks}
-                                    styles={{ markLabel: { display: 'none' } }}
-                                />
-                                <Text size="sm" mt="xl">Speed of Each Core</Text>
-                                <Slider defaultValue={1} min={1} max={10} step={1} label={(value) => `${value} Gflop/sec`} />
-                            </Fieldset>
-                        </Tabs.Panel>
-                    ))}
-                </Tabs>
+                            {tabs.map((tab) => (
+                                <Tabs.Panel key={tab.id} value={tab.id}>
+                                    <SimpleGrid cols={2} pt={15}>
+                                        <div>
+                                            <Fieldset legend="Cluster Link Specifications">
+                                                <Text size="sm" mt="xl">Bandwidth</Text>
+                                                <Slider defaultValue={100} min={50} max={1000} step={50} label={(value) => `${value} kBps`} />
+                                                <Text size="sm" mt="xl">Latency</Text>
+                                                <Slider defaultValue={10} min={1} max={100} step={1} label={(value) => `${value} ms`} />
+                                            </Fieldset>
+                                        </div>
+                                        <div>
+                                            <Fieldset legend="Compute Node Specifications">
+                                                <Text size="sm" mt="xl">Number of Compute Nodes</Text>
+                                                <Slider defaultValue={1} min={1} max={10} step={1} />
+                                                <Text size="sm" mt="xl">Cores per Compute Node</Text>
+                                                <Slider
+                                                    defaultValue={50}
+                                                    label={(val) => marks.find((mark) => mark.value === val)!.label}
+                                                    step={25}
+                                                    marks={marks}
+                                                    styles={{ markLabel: { display: 'none' } }}
+                                                />
+                                                <Text size="sm" mt="xl">Speed of Each Core</Text>
+                                                <Slider defaultValue={1} min={1} max={10} step={1} label={(value) => `${value} Gflop/sec`} />
+                                            </Fieldset>
+                                        </div>
+                                    </SimpleGrid>
+                                </Tabs.Panel>
+                            ))}
+                        </Tabs>
+                    </Group>
+                </Fieldset>
             </Group>
         </Modal>
     );
