3e5faa4220f110756223684962f1044aa22be69a
Jianlong Chen <jianlong@hawaii.edu>
2025-03-25 19:32:39 -1000

    test

--- api/run_uvicorn_and_wrench_daemon.bash
+++ api/run_uvicorn_and_wrench_daemon.bash
@@ -1,11 +1,19 @@
 #!/usr/bin/bash
 
+IPINFO_TOKEN_SECRET_FILE="/run/secrets/ipinfo_token_file"
+if [ ! -f $IPINFO_TOKEN_SECRET_FILE ]; then
+  echo "Error: $IPINFO_TOKEN_SECRET_FILE does not exist!" >&2
+  exit 1  # Exit with an error code
+elif [ ! -s $IPINFO_TOKEN_SECRET_FILE ]; then
+  echo "Error: $IPINFO_TOKEN_SECRET_FILE is empty!" >&2
+  exit 1  # Exit with an error code
+else
+  # File exists and is not empty, read the content into the variable
+  export IPINFO_TOKEN=$(cat $IPINFO_TOKEN_SECRET_FILE)
+fi
+
 echo "Launching wrench-daemon..."
 wrench-daemon --num-commports 50000 &
 
 echo "Launching uvicorn..."
 uvicorn src.main:app --host 0.0.0.0 --port 8081  --reload
-
-
-
-
